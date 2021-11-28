3314d03036ed71d6efcddf1ec4268ff9694d5ea1
Manuel Zarat <manuel@zarat.ml>
2021-11-28 12:01:35 +0100

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -3,17 +3,6 @@
 
 #include "Jail.h"
 
-namespace JAIL {
-
-    extern void registerChar(JInterpreter* interpreter);
-    extern void registerInteger(JInterpreter* interpreter);
-    extern void registerDouble(JInterpreter* interpreter);
-    extern void registerString(JInterpreter* interpreter);
-    extern void registerArray(JInterpreter* interpreter);
-    extern void registerObject(JInterpreter* interpreter);
-
-};
-
 void print(JAIL::JObject *c, void *data) {
 
     std::cout << c->getParameter("str")->getString();  
